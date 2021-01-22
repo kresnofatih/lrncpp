7e293db86fbcedb953d74d9dd7b2574222e70bb1
Fatih <kresnofatihimani.private@gmail.com>
2021-01-22 19:25:44 +0700

    order sequence case

--- cpp1.cpp
+++ cpp1.cpp
@@ -2,10 +2,21 @@
 using namespace std;
 
 int main(){
-    int a, i;
-    cin >> a;
-    for (i=0; i<=a; i++){
-        cout << i << endl;
+    int i, j, temp, b[5];
+    for(i=0; i<5; i++){
+        b[i]=i;// assign array values
+    };
+    for(i=0; i<5; i++){
+        for(j=0; j<5; j++){
+            if(b[j]<b[j+1]){
+                temp=b[j+1];
+                b[j+1]=b[j];
+                b[j]=temp;
+            }
+        }
     }
+    for(i=0;i<5;i++){
+        cout<<b[i]<<endl;
+    };
     return 0;
 }
