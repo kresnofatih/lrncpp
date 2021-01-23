2cee4413363184dd83fa19198ddbfd1869aa240c
Fatih <kresnofatihimani.private@gmail.com>
2021-01-23 20:24:35 +0700

    created Function for switch case

--- cpp2.cpp
+++ cpp2.cpp
@@ -1,26 +1,35 @@
 #include <iostream>
 using namespace std;
 
+float getResult(float a, float b, int op);
+
 int main(){
     float a, b, result;
     int op;
     cin >> a;
     cin >> b;
     cin >> op;
-    switch(op){
-        case 0:
-            result = a+b;
-            break;
-        case 1:
-            result = a-b;
-            break;
-        case 2:
-            result = a*b;
-            break;
-        case 3:
-            result = a/b;
-            break;
-    }
+    result = getResult(a,b,op);
     cout << result << endl;
     return 0;
+}
+
+
+float getResult(float a, float b, int op){
+    float res;
+    switch(op){
+        case 0:
+            res= (a+b);
+            break;
+        case 1:
+            res= (a-b);
+            break;
+        case 2:
+            res= (a*b);
+            break;
+        case 3:
+            res= (a/b);
+            break;
+    };
+    return res;
 }
