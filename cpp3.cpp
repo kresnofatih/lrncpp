9d2afeff0e7d52ac917bff006b499f9b1a159c7c
Fatih <kresnofatihimani.private@gmail.com>
2021-04-10 08:26:08 +0700

    arithmatics :panda_face:

--- cpp3.cpp
+++ cpp3.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
-#include <limits>
 
 int main(){
-    int a = 1;
-    short b = 8;
-    float c = 3.3;
-    std::cout << a <<std::endl;
-    std::cout << sizeof(a) << std::endl;
-    std::cout << std::numeric_limits<int>::max() << std::endl;
-    std::cout << std::numeric_limits<int>::min() << std::endl;
-    std::cout << a <<std::endl;
-    std::cout << sizeof(a) << std::endl;
-    std::cout << std::numeric_limits<unsigned int>::max() << std::endl;
-    std::cout << std::numeric_limits<unsigned int>::min() << std::endl;
-    std::cout << b <<std::endl;
-    std::cout << sizeof(b) << std::endl;
-    std::cout << std::numeric_limits<short>::max() << std::endl;
-    std::cout << std::numeric_limits<short>::min() << std::endl;
-    std::cout << c <<std::endl;
-    std::cout << sizeof(c) << std::endl;
-    std::cout << std::numeric_limits<float>::max() << std::endl;
-    std::cout << std::numeric_limits<float>::min() << std::endl;
+    int a = 4;
+    int b = 3;
+
+    std::cout << a+b << std::endl;//7
+    std::cout << sizeof(a+b) << std::endl;//4
+    int c = 7;
+    short d = 12;
+
+    std::cout << c+d << std::endl;//19
+    std::cout << sizeof(c+d) << std::endl;//4
+    std::cout << d/c << std::endl;//1
+    std::cout << sizeof(d/c) << std::endl;//4
+
+    double e = 3.2;
+    int f = 9;
+    float g = f/e;
+
+    std::cout << f/e << std::endl;// 2.8125
+    std::cout << sizeof(f/e) << std::endl;//8
+    std::cout << g << std::endl;// 2.8125
+    std::cout << sizeof(g) << std::endl;//4
     return 0;
 }
