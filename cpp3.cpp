580278a1e6faf2b60bd21ba03b170461b9c3942c
Fatih <kresnofatihimani.private@gmail.com>
2021-04-10 08:38:13 +0700

    komparasi :cat:

--- cpp3.cpp
+++ cpp3.cpp
@@ -1,26 +1,21 @@
 #include <iostream>
 
 int main(){
-    int a = 4;
-    int b = 3;
+    int a = 1;
+    int b = 9;
 
-    std::cout << a+b << std::endl;//7
-    std::cout << sizeof(a+b) << std::endl;//4
-    int c = 7;
-    short d = 12;
-
-    std::cout << c+d << std::endl;//19
-    std::cout << sizeof(c+d) << std::endl;//4
-    std::cout << d/c << std::endl;//1
-    std::cout << sizeof(d/c) << std::endl;//4
-
-    double e = 3.2;
-    int f = 9;
-    float g = f/e;
-
-    std::cout << f/e << std::endl;// 2.8125
-    std::cout << sizeof(f/e) << std::endl;//8
-    std::cout << g << std::endl;// 2.8125
-    std::cout << sizeof(g) << std::endl;//4
+    std::cout << (a==b) << std::endl;//0
+    std::cout << sizeof(a==b) << std::endl;//1
+    std::cout << (a!=b) << std::endl;//1
+    std::cout << sizeof(a==b) << std::endl;//1
+    std::cout << (a&&b) << std::endl;//1
+    std::cout << sizeof(a&&b) << std::endl;//1
+    std::cout << std::endl;
+    bool c = true;
+    bool d = false;
+    std::cout << (c&&d) << std::endl;//0
+    std::cout << sizeof(c&&d) << std::endl;//1
+    std::cout << (c||d) << std::endl;//1
+    std::cout << sizeof(c||d) << std::endl;//1
     return 0;
 }
