c1e800093f0c17569dd2d32df625811f12ddf0dc
Fatih <kresnofatihimani.private@gmail.com>
2021-04-11 06:48:39 +0700

    switchcase :maple_leaf:

--- cpp3.cpp
+++ cpp3.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 
 int main(){
-    int a = 7;
+    int a = 2;
 
-    if (a==4){
-        std::cout << "a==4" << std::endl;
-    } else if(a==7){
-        std::cout << "a==7" << std::endl;
-    } else {
-        std::cout << "a!=4" << std::endl;
+    switch(a){
+        case 1:
+            std::cout << "a==1" << std::endl;
+            break;
+        case 2:
+            std::cout << "a==2" << std::endl;
+            break;
+        case 0:
+            std::cout << "a==0" << std::endl;
+            break;
     }
     return 0;
 }
