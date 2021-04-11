a2d9a0bc8364da20581a223dd0d65d99d000fe03
Fatih <kresnofatihimani.private@gmail.com>
2021-04-11 10:48:51 +0700

    loops done :snake::fish:

--- cpp3.cpp
+++ cpp3.cpp
@@ -1,21 +1,12 @@
 #include <iostream>
 
 int main(){
-    int a = 5;
-    while(a<=10){
-        std::cout << "a: " << a << std::endl;
-        a++;
-    }
-
-    int b = 5;
-    
-    do{
-        std::cout << "b: " << b << std::endl;
-        b++;
-    } while(b<=10);
-
-    for(int c=5; c<=10; c++){
-        std::cout << "c: " << c << std::endl;
+    for(int a = 0; a<=10; a++){
+        if(a==5){
+            // break; //finishes loop
+            continue; // skips that loop
+        }
+        std::cout << a << std::endl;
     }
     return 0;
 }
