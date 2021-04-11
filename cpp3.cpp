3af71c63d14c9ab8f4d2d8d97e0657992ebcfd00
Fatih <kresnofatihimani.private@gmail.com>
2021-04-11 11:16:04 +0700

    fibonacci :tiger:

--- cpp3.cpp
+++ cpp3.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 
 int main(){
-    for(int a = 0; a<=10; a++){
-        if(a==5){
-            // break; //finishes loop
-            continue; // skips that loop
+    int finalIndex = 10;
+    int b, c, d;
+
+    for(int a = 0; a<=finalIndex; a++){
+        if (a==0){
+            b=1;
+            std::cout << b << " ";
+        } else if(a==1){
+            b=1;
+            c=1;
+            std::cout << b << " ";
+        } else {
+            d=b;
+            b=b+c;
+            c=d;
+            std::cout << b << " ";
         }
-        std::cout << a << std::endl;
     }
     return 0;
 }
