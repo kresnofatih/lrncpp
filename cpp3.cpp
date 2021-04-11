e94b3417eba9324b06ab69f13ac18fddb6fe521c
Fatih <kresnofatihimani.private@gmail.com>
2021-04-11 10:42:05 +0700

    loops :frog:

--- cpp3.cpp
+++ cpp3.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 
 int main(){
-    int a = 2;
+    int a = 5;
+    while(a<=10){
+        std::cout << "a: " << a << std::endl;
+        a++;
+    }
 
-    a++;
-    std::cout << a << std::endl;//3
-    a--;
-    std::cout << a << std::endl;//2
-    // a;
-    std::cout << ++a << std::endl;//3
-    // a--;
-    std::cout << --a << std::endl;//2
+    int b = 5;
+    
+    do{
+        std::cout << "b: " << b << std::endl;
+        b++;
+    } while(b<=10);
+
+    for(int c=5; c<=10; c++){
+        std::cout << "c: " << c << std::endl;
+    }
     return 0;
 }
