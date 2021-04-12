50efcb82bc21e6cef9f56f3611675411c3fca984
Fatih <kresnofatihimani.private@gmail.com>
2021-04-12 19:14:11 +0700

    rekursif faktorial :shaved_ice:

--- cpp3.cpp
+++ cpp3.cpp
@@ -1,27 +1,15 @@
 #include <iostream>
 
-int pangkatIterasi(int a, int b){
-    int hasil = a;
-    for (int i = 1; i<b; i++){
-        hasil *= a;
-    }
-    return hasil;
-}
-
-int pangkatRekursif(int a, int b){
-    if(b==1){
+int faktorial(int a){
+    if(a==1){
         return a;
     } else {
-        return a*pangkatRekursif(a, b-1);
+        return a*faktorial(a-1);
     }
 }
 
 int main(){
-    int a, b;
-    a=2;
-    b=4;
-    std::cout << pangkatIterasi(a, b) << std::endl;
-    std::cout << pangkatRekursif(a, b) << std::endl;
     
+    std::cout << faktorial(4) << std::endl;
     return 0;
 }
