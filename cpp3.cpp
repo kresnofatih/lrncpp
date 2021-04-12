fbebb2018fc05ff90c6846cbc8d2c72b3c7e53f0
Fatih <kresnofatihimani.private@gmail.com>
2021-04-12 19:07:22 +0700

    pangkat :dragon:

--- cpp3.cpp
+++ cpp3.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 
-// basic function
-int boxArea(int p, int l){
-    return p*l;
+int pangkatIterasi(int a, int b){
+    int hasil = a;
+    for (int i = 1; i<b; i++){
+        hasil *= a;
+    }
+    return hasil;
 }
 
-// overload function
-int boxArea(int p){
-    return p*p;
-}
-
-float boxArea(float p){
-    return p*p;
+int pangkatRekursif(int a, int b){
+    if(b==1){
+        return a;
+    } else {
+        return a*pangkatRekursif(a, b-1);
+    }
 }
 
 int main(){
-    std::cout << boxArea(2, 4) << std::endl;
-    std::cout << boxArea(2) << std::endl;
-    std::cout << boxArea(3.3f) << std::endl;
+    int a, b;
+    a=2;
+    b=4;
+    std::cout << pangkatIterasi(a, b) << std::endl;
+    std::cout << pangkatRekursif(a, b) << std::endl;
+    
     return 0;
 }
