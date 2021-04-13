c256f32bfac4227b64c278e0e40d70b0921ed302
Fatih <kresnofatihimani.private@gmail.com>
2021-04-13 07:09:21 +0700

    passing reference :cat:

--- cpp3.cpp
+++ cpp3.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 
+void incrementByTwo(int &x){
+    x++;
+    x++;
+}
+
+void incrementByThree(int x){
+    x++;
+    x++;
+    x++;
+    std::cout << &x << std::endl;
+    std::cout << x << std::endl;
+}
+
 int main(){
-    int a = 3;
-    int &b = a;
-    a = 5;
-    std::cout << a << " " << b << std::endl;
+    int a=9;
+    incrementByTwo(a);
+    std::cout << a << std::endl;
+    std::cout << &a << std::endl; //0x61fe1c
+    incrementByThree(a); // 0x61fdf0
     return 0;
 }
